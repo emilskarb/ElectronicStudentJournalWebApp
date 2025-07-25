796b187535f0f13c19b5be229fd7fcbed42b099f
Emil <Praktykant_3@zetorzeszow.pl>
2025-07-25 11:50:23 +0200

    Dodano model ucznia

---
+++ ElectronicStudentJournal/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace ElectronicStudentJournal.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string FisrtName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int NumerPesel { get; set; }
+
+        public int NumerTelefonu { get; set; }
+
+        public string AdresZamieszkania { get; set; }
+    }
+}
