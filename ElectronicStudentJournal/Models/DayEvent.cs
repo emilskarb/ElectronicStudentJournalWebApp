f98c4aca08e00db72240d309fbc37670d8c3162d
Emil <Praktykant_3@zetorzeszow.pl>
2025-08-19 13:59:12 +0200

    Dodano kalendarz i poprawiono integrację z tailwind

---
+++ ElectronicStudentJournal/Models/DayEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace ElectronicStudentJournal.Models;
+
+public class DayEvent
+{
+	public int DayEventId { get; set; }
+	public string Note { get; set; }
+	public DateTime EventDate { get; set; } = new DateTime(1900, 1, 1);
+	public DateTime FromDate { get; set; } = new DateTime(1900, 1, 1);
+	public DateTime ToDate { get; set; } = new DateTime(1900, 1, 1);
+	public int DateValue { get; set; }
+	public string DayName { get; set; }
+	public string Message { get; set; }
+
+}
---
+++ ElectronicStudentJournal/Models/WeekClass.cs
@@ -0,0 +1,6 @@
+﻿namespace ElectronicStudentJournal.Models;
+
+public class WeekClass
+{
+	public List<DayEvent> Dates { get; set; } = new List<DayEvent>();
+}
