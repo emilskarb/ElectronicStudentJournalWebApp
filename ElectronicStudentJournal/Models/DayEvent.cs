1e69c5b38911db99f457559409fdd2ff84bf84ba
Emil <Praktykant_3@zetorzeszow.pl>
2025-08-20 13:59:35 +0200

    Poprawiono kalendarz

--- ElectronicStudentJournal/Models/DayEvent.cs
+++ ElectronicStudentJournal/Models/DayEvent.cs
@@ -7,7 +7,7 @@ public class DayEvent
 	public DateTime EventDate { get; set; } = new DateTime(1900, 1, 1);
 	public DateTime FromDate { get; set; } = new DateTime(1900, 1, 1);
 	public DateTime ToDate { get; set; } = new DateTime(1900, 1, 1);
-	public int DateValue { get; set; }
+	public string DateValue { get; set; }
 	public string DayName { get; set; }
 	public string Message { get; set; }
 
